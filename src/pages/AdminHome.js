4da901e20db10ecb7b5f3b599f1292ba1ff4cf84
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-06 21:27:46 +0530

    Create admin home

---
+++ src/pages/AdminHome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GenaralNav from "../components/GeneralNav";
+
+function AdminHome() {
+  return (
+    <div>
+      <GenaralNav />
+      <section className="homecontainer">
+        <h1>This is Admin User Home</h1>
+      </section>
+    </div>
+  );
+}
+
+export default AdminHome;
