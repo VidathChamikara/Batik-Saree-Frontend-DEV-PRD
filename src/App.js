4500f9cbb5c01a059ab5f8cef58233ff966c7fd0
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-06 21:25:25 +0530

    logged or not checking

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Landing from "./pages/Landing";
-import LoginSignup from "./pages/LoginSignup";
+
+//Components
+import GeneralNav from "./components/GeneralNav";
 import ShirtDesigner from "./pages/ShirtDesigner";
 
+//Pages
+import Landing from "./pages/Landing";
+import LoginSignup from "./pages/LoginSignup";
+import UserDetails from "./pages/userDetails";
+import GenaralHome from "./pages/GeneralHome";
+import AdminHome from "./pages/AdminHome";
+
 function App() {
+  const isLoggedIn = window.localStorage.getItem("loggedIn");
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Landing />} />
-        <Route path="/loginSignup" element={<LoginSignup />} />       
+        
+        <Route path="/" element={<Landing />} />
+        <Route exact path="/loginSignup" element={isLoggedIn=="true"?<UserDetails/>:<LoginSignup/>} />
+        <Route path="/loginSignup" element={<LoginSignup />} />
+        <Route path="/generalHome" element={<GenaralHome />} />
+        <Route path="/adminHome" element={<AdminHome />} />
+        <Route path="/userDetails" element={<UserDetails />} />
+
         <Route path="/shirt" element={<ShirtDesigner />} />
-        {/* Other routes for your application */}
+        <Route path="/generalNav" element={<GeneralNav />} />
       </Routes>
     </Router>
   );
