3d3c74c461f682a6ba0890def26179a506281b7a
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-06 21:27:28 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import "font-awesome/css/font-awesome.min.css"; // Font Awesome styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
