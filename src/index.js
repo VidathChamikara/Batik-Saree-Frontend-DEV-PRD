35fb0417ff108acf962d9457448ac09da5d31885
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-07 15:38:40 +0530

    Import css link

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "font-awesome/css/font-awesome.min.css"; // Font Awesome styles
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
