709007bfb4d42eb565f7c63e06e8ca8d3d290b5f
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-08 08:15:02 +0530

    Welcome Text

--- src/pages/AdminHome.js
+++ src/pages/AdminHome.js
@@ -6,7 +6,15 @@ function AdminHome() {
     <div>
       <GenaralNav />
       <section className="homecontainer">
-        <h1>This is Admin User Home</h1>
+      <div className="content__homecontainer">
+          <h1>
+            Hey Admin !!!
+            <br />
+            <span className="heading__1">Welcome to the</span>
+            <br />
+            <span className="heading__2">Batik Saree Admin Panel</span>
+          </h1>
+        </div>
       </section>
     </div>
   );
--- src/pages/GeneralHome.js
+++ src/pages/GeneralHome.js
@@ -2,18 +2,21 @@ import React from "react";
 import GenaralNav from "../components/GeneralNav";
 
 function GenaralHome() {
- 
   return (
-   <div>
-    <GenaralNav/>
-    <section className="homecontainer">
-    <div className="content__homecontainer">
-    <h1>
-        This is General User Home
-    </h1>
+    <div>
+      <GenaralNav />
+      <section className="homecontainer">
+        <div className="content__homecontainer">
+          <h1>
+            Hey Vidath !!!
+            <br />
+            <span className="heading__1">Welcome to our</span>
+            <br />
+            <span className="heading__2">Batik Saree</span>
+          </h1>
+        </div>
+      </section>
     </div>
-    </section>
-   </div>
   );
 }
 
