753715ab6f2d66c5af746766aef35bd4d0b398be
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-08 10:13:35 +0530

    Welcome text of admin home

--- src/pages/AdminHome.js
+++ src/pages/AdminHome.js
@@ -6,14 +6,21 @@ function AdminHome() {
     <div>
       <GenaralNav />
       <section className="homecontainer">
-      <div className="content__homecontainer">
+        <div className="content__homecontainer">
           <h1>
-            Hey Admin !!!
+            Hey Admin 👋
             <br />
             <span className="heading__1">Welcome to the</span>
             <br />
-            <span className="heading__2">Batik Saree Admin Panel</span>
+            <span className="heading__2">Batik Admin Panel</span>
           </h1>
+          <p style={{ textAlign: "justify" }}>
+            Welcome to the Batik Admin Panel! Dive into a world where you have the
+            power to customize every aspect of your design. From selecting
+            intricate patterns to managing your clientele, unleash your
+            creativity and take control of your Batik journey. Let's craft
+            unforgettable creations together!
+          </p>
         </div>
       </section>
     </div>
