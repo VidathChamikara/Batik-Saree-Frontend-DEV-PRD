8f46b58cb8907c470f4704663852470ac6870b96
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-08 09:59:13 +0530

    Update general user welcome text

--- src/pages/GeneralHome.js
+++ src/pages/GeneralHome.js
@@ -8,12 +8,18 @@ function GenaralHome({ userName }) {
       <section className="homecontainer">
         <div className="content__homecontainer">
           <h1>
-            Hey {userName} !!!
+            Hey {userName} 👋
             <br />
             <span className="heading__1">Welcome to our</span>
             <br />
-            <span className="heading__2">Batik Saree</span>
+            <span className="heading__2">Batik Studio !!!</span>
           </h1>
+          <p style={{ textAlign: "justify" }}>
+            Welcome to our creative space! Imagine yourself as the designer of
+            your dream saree. Let your imagination run wild as you embark on a
+            journey to bring your unique vision to life. Let's begin weaving
+            your masterpiece together!
+          </p>
         </div>
       </section>
     </div>
