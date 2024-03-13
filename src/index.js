93a54af8a50daf31c1ac134546d4c151451e850b
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-13 15:14:56 +0530

    Minor Change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import "font-awesome/css/font-awesome.min.css"; // Font Awesome styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
