374ed59d43290ec39c27174d1c9783f720ddad78
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-05-12 18:18:23 +0530

    Create user.js

---
+++ src/pages/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GenaralNav from "../components/GeneralNav";
+import SareeDesigner from "./SareeDesigner";
+
+function User() {
+  return (
+    <div>
+      <GenaralNav />
+      <SareeDesigner />
+    </div>
+  );
+}
+
+export default User;
